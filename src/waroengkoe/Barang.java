5bc8ad32cf02c50d06fef899077ab4d1c3ef8953
Indra Ginanjar <indraginanjar@gmail.com>
2011-12-31 08:16:06 +0700

    Commit pertama

---
+++ src/waroengkoe/Barang.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package waroengkoe;
+
+/**
+ *
+ * @author indra
+ */
+public class Barang {
+    public String nama;
+    public String harga;
+    public String persediaan;
+}
---
+++ src/waroengkoe/DaftarBarangDB.java
@@ -0,0 +1,43 @@
+package waroengkoe;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author indra
+ */
+public class DaftarBarangDB extends DaftarDB<Barang> {
+    
+    public DaftarBarangDB(Connection connection){
+        super(connection);
+    }
+    
+    /**
+     * Menambahkan barang ke tabel barang di basis data
+     * @param namaBarang
+     * @param harga
+     * @param persediaan
+     * @return true -- jika berhasil, false -- jika gagal (muncul exception)
+     */
+    @Override
+    public boolean tambah(Barang barang){
+        return false;
+    }
+    
+    @Override
+    public void ubah(Barang barang){
+        
+    }
+    
+    @Override
+    public void hapus(String key){
+        
+    }
+    
+    public ResultSet ambilDaftar(){
+        return super.ambilDaftar("barang");
+    }
+}
